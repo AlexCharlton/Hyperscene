02f85d78fe1b6349d2f713fef294da2a024e69ee
Alex Charlton <alex.n.charlton@gmail.com>
2015-01-10 21:39:47 -0500

    Fix template

--- extensionTemplate.c
+++ extensionTemplate.c
@@ -14,17 +14,17 @@ void XpreRender(void *data){
 void XpostRender(void *data){
 }
 
-void XvisibleNode(void *data, HPGnode *node){
+void XvisibleNode(void *data, HPSnode *node){
 }
 
-void XupdateNode(void *data, HPGnode *node){
+void XupdateNode(void *data, HPSnode *node){
 }
 
-HPGextension x = {initX,
+HPSextension x = {initX,
                   XpreRender,
                   XpostRender,
                   XvisibleNode,
                   XupdateNode,
                   deleteX};
 
-HPGextension *X = &x; //external 
+HPSextension *X = &x; //external 
