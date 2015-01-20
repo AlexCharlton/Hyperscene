1d9e2bb48704b96fadcf961d53fe7820047631fc
Alex Charlton <alex.n.charlton@gmail.com>
2015-01-19 21:27:09 -0500

    Squashed 'hypermath/' changes from 95a9f83..c028de0
    c028de0 Clarify hpmPerspective angle parameter
    ae0bf8c Another typo
    6a67ce8 README typo
    da98e88 Fix argument order in hpm***Viewport functions
    1a8767f Document version 0.7.0 changes
    cf5bd5a Document
    08a9249 Add hpmFrustumViewport, hpmOrthoViewport
    
    git-subtree-dir: hypermath
    git-subtree-split: c028de0f39326bbc50bb5c6b0a847933335b15c2

--- include/hypermath.h
+++ include/hypermath.h
@@ -144,11 +144,20 @@ void hpmFastInverseTranspose(const float *mat, float *result);
 // Projection
 void hpmOrtho(int width, int height, float near, float far, float *mat);
 
+void hpmOrthoViewport(float left, float right, float bottom, float top,
+                      float near, float far,
+                      float vLeft, float vRight, float vBottom, float vTop,
+                      float *mat);
+
+void hpmPerspective(int width, int height, float near, float far, float angle, float *mat);
+
 void hpmFrustum(float left, float right, float bottom, float top,
 		float near, float far, float *mat);
 
-void hpmPerspective(int width, int height, float near, float far, float angle,
-		    float *mat);
+void hpmFrustumViewport(float left, float right, float bottom, float top,
+                        float near, float far,
+                        float vLeft, float vRight, float vBottom, float vTop,
+                        float *mat);
 
 // Camera
 void hpmLookAt(float *eye, float *obj, float *up, float *mat);
